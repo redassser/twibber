32da0579457349c79c553f032d0ab4cbdfffce89
redassser <redryan890@gmail.com>
2023-07-23 19:05:49 -0400

    Left side of home ui looking good

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,41 +3,46 @@ import React from "react"
 import {
   BiLogoTwitter, BiSolidHomeAlt2, BiSolidBulb, BiSolidBell,
   BiSolidChat, BiSolidDetail, BiSolidBookmark,
-  BiSolidHappy, BiSolidBadgeCheck, BiSolidUserAccount, BiSolidRuler
+  BiDotsHorizontalRounded, BiSolidUserAccount
 } from "react-icons/bi"
 
 const navItems = [
+  { title: "", icon: BiLogoTwitter, exclude: true},
   { title: "Home", icon: BiSolidHomeAlt2 },
   { title: "Explore", icon: BiSolidBulb },
   { title: "Notifications", icon: BiSolidBell },
   { title: "Messages", icon: BiSolidChat },
   { title: "Lists", icon: BiSolidDetail },
   { title: "Bookmarks", icon: BiSolidBookmark },
-  { title: "Communities", icon: BiSolidHappy },
-  { title: "Verified", icon: BiSolidBadgeCheck },
   { title: "Profile", icon: BiSolidUserAccount },
-  { title: "More", icon: BiSolidRuler },
+  { title: "More", icon: BiDotsHorizontalRounded },
 ]
 
 const page = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center relative bg-black">
-      <div className="max-w-screen-lg w-full h-full flex relative">
+    <div className="w-full h-full flex justify-center items-center relative bg-black ">
+      <div className="max-w-screen-xl w-full h-full flex relative">
         {/* Left sidebar */}
-        <section className="fixed w-72 flex flex-col">
-          <Link href={"/"} className="my-4"><BiLogoTwitter/></Link>
-          {
-            navItems.map((items) => (
-              <Link className="bg-white/50 rounded-3xl p-4 flex items-center justify-center space-x-2" href={"/"+items.title.toLowerCase()} key={items.title}>
-                <div><items.icon /></div>
-                <div>
-                  {
-                    items.title
-                  }
-                </div>
+        <section className="fixed w-275px flex flex-col items-stretch h-screen">
+          <div className="flex flex-col items-stretch h-screen space-y-4 mt-4">
+            {navItems.map((item) => (
+              <Link className="hover:bg-white/10 text-xl transition duration-200 rounded-3xl py-2 px-6 flex items-center justify-start w-fit space-x-6" href={"/"+item.title.toLowerCase()} key={item.title}>
+                <div><item.icon /></div>
+                {!item.exclude && <div>{item.title}</div>}
               </Link>
-            ))
-          }
+            ))}
+            <button className="w-full rounded-full bg-primary p-4 text-xl text-center hover:bg-opacity-80 transition duration-200">Tweet</button>
+          </div>
+          <button className="flex items-center space-x-2 m-4 rounded-full p-4 w-full justify-between text-xl text-cetner hover:bg-white/10 transition duration-200">
+            <div className="flex items-center space-x-2">
+              <div className="rounded-full w-10 h-10 bg-white"></div>
+              <div className="text-left">
+                <div className="text-sm font-semibold">pie</div>
+                <div className="text-xs">@byebyepiepie</div>
+              </div>
+            </div>
+            <div><BiDotsHorizontalRounded/></div>
+          </button>
         </section>
         {/* Infinite Scroll */}
         <main></main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      "white": "#fff",
+      "primary": "#ada344"
+    }
   },
   plugins: [],
 }
